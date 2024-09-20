25ddc775a6a6b4df39218d227f6834b5b5c9d059
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-09-20 08:23:30 -0500

    comunicacion entre componente

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,12 +1,66 @@
+import { useState } from "react"
+import AddCategory from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
+    const handleAdd = () => {
+        setCategories([ 'HunterXHunter',...categories])
+    }
 
-return (
-    <>
-        <h2>GifExpertApp</h2>
-        <hr />
-    </>
-)
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+           
+           <AddCategory
+            setCategories={setCategories}
+           
+           />
+            <hr />
+            {/* <button onClick={handleAdd}>Agregar</button> */}
+            <hr />
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}>
+                            {category}
+                        </li>
+                    })
+                }
+            </ol>
+            <hr />
+        </>
+    )
 
 }
+
+
+
+
+
+
+
+
+
+
+
+// const apiKey = 'fxpQClUSxdDsbEJGmBB7RgXLWyAv6ouP';
+// const serviceUrl = 'https://api.giphy.com/v1/gifs/'
+
+// const getImage = fetch(`${serviceUrl}random?api_key=${apiKey}`)
+
+
+
+
+
+// getImage
+//   .then(response => response.json())
+//   .then(({ data }) => {
+//     const { url } = data.images.original
+//     // console.log(data.images.original.url)
+//     const img = document.createElement('img');
+//     img.src = url;
+//     document.body.append(img)
+
+
+//   }).catch(console.warn);
