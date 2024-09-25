5ae06eef45891c0695ddc145debfd3e0c20915f7
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-09-25 09:45:16 -0500

    refactor

--- sReact/proyect-1/src/ContadorApp.jsx
+++ sReact/proyect-1/src/ContadorApp.jsx
@@ -1,37 +1,15 @@
 import { useState } from "react";
-
-
-
 export const ContadorApp = ({ value }) => {
-
     const [contador, setContador] = useState(value)
-    const onIncrement = () => {
-        setContador(contador + 1)
-    }
-    const onDecrement = () => {
-        setContador(contador - 1)
-    }
-    const onReset = () => {
-        setContador(value)
-    }
-
-
-
-
-
-
+    const onIncrement = () => setContador(contador + 1)
+    const onDecrement = () => setContador(contador - 1)
+    const onReset = () => setContador(value)
     return (
         <>
             <h1>{contador}</h1>
-            <button
-                onClick={onIncrement}
-            >+1</button>
-            <button
-                onClick={onDecrement}
-            >-1</button>
-            <button
-                onClick={onReset}
-            >Reset</button>
+            <button onClick={onIncrement} >+1</button>
+            <button onClick={onDecrement}>-1</button>
+            <button onClick={onReset}>Reset</button>
         </>
     )
 }
