f00440c1d217a651b5a5088d9ffb1dad7610d9e5
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-09-25 09:36:59 -0500

    contador

---
+++ sReact/proyect-1/src/ContadorApp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+
+export const ContadorApp = ({ value }) => {
+
+    const [contador, setContador] = useState(value)
+    const onIncrement = () => {
+        setContador(contador + 1)
+    }
+    const onDecrement = () => {
+        setContador(contador - 1)
+    }
+    const onReset = () => {
+        setContador(value)
+    }
+
+
+
+
+
+
+    return (
+        <>
+            <h1>{contador}</h1>
+            <button
+                onClick={onIncrement}
+            >+1</button>
+            <button
+                onClick={onDecrement}
+            >-1</button>
+            <button
+                onClick={onReset}
+            >Reset</button>
+        </>
+    )
+}
---
+++ sReact/proyect-1/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles.css'
+import  {ContadorApp } from './ContadorApp'
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <ContadorApp 
+    
+    value={0}
+    />
+
+  </React.StrictMode>,
+)
