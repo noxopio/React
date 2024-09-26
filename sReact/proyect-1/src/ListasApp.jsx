f693255c81d1ad64ba3784ffb470b5d991a0ef4e
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-09-26 13:12:51 -0500

    listas

--- sReact/proyect-1/src/ListasApp.jsx
+++ sReact/proyect-1/src/ListasApp.jsx
@@ -5,7 +5,7 @@ const Items = ({ nombre, visto }) => {
     return (<li>{nombre}{visto ? '✅' : '❌'}</li>)
 }
 export const ListasApp = () => {
-  
+
     const addTask = () => {
         setArreglo([...arreglo, { index: 3, nombre: 'Componentes', visto: false }])
     }
@@ -14,17 +14,15 @@ export const ListasApp = () => {
         { index: 2, nombre: 'Uso de vite', visto: false },
     ]
     const [arreglo, setArreglo] = useState(listadoSecciones)
-
     const onAgregarTarea = (val) => {
         let valor = val.trim()
-        if(valor<1)return
+        if (valor < 1) return
         const envio = {
             nombre: valor,
             visto: false,
-            index:arreglo.length+1,
+            index: arreglo.length + 1,
         }
         setArreglo([...arreglo, envio])
-        
     }
     return (
         <>
@@ -32,9 +30,11 @@ export const ListasApp = () => {
             <h1>Listados </h1>
             <ol>{arreglo.map(({ visto, nombre, index }) => <Items key={index} nombre={nombre} visto={visto} />)}
             </ol>
-
-            {/* <button onClick={addTask} >
-                agregar            </button> */}
+            {/* 
+            <button onClick={addTask} >
+                agregar
+            </button> */
+            }
         </>
     )
 }
