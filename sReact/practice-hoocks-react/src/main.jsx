b57c775bc9257e946755559c6a7d23d9d3f84e36
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-10-07 17:06:14 -0500

    uso de fetch con desestructuracion

--- sReact/practice-hoocks-react/src/main.jsx
+++ sReact/practice-hoocks-react/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HooksApp } from "./HooksApp";
 import "./index.css";
-import { FormularioComponent } from "./components/FormularioComponent";
-import { UsersComponent } from "./components/UsersComponent";
+// import { FormularioComponent } from "./components/FormularioComponent";
+// import { UsersComponent } from "./components/UsersComponent";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
