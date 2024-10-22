d94f3f1d919950f6e003b9bf05d84200b9af99cd
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-10-22 14:12:39 -0500

    useFetch con cache

--- 05-hooks-app/src/hooks/useFetch.js
+++ 05-hooks-app/src/hooks/useFetch.js
@@ -1,66 +1,88 @@
-import { useEffect, useState } from "react"
-export const useFetch = (url) => {
-    const [state, setState] = useState(
-        {
-            data: null,
-            isLoading: true,
-            hasError: false,
-            error: null,
-        }
-    )
+import { useEffect, useState } from 'react';
 
-    useEffect(() => {
-        getFecth();
-    }, [url])
-    const setLoadingState = () => {
-        setState({
-            data: null,
-            isLoading: true,
-            hasError: false,
-            error: null
-        })
-    }
-    const getFecth = async () => {
-    
-        try {
-            // const url = 'https://pokeapi.co/api/v2/pokemon/1';
-            const res = await fetch(url)
-            await new Promise(resolve => setTimeout(resolve, 1500));
-            if (!res.ok) {
-                setState({
-                    data: null,
-                    isLoading: false,
-                    hasError: true,
-                    error: {
-                        status: res.status,
-                        statusText: !res.statusText ? 'Ocurrio un error' : res.statusText,
-                    }
-                })
-                return;
-            }
-            const data = await res.json();
-            setState({
-                data,
-                isLoading: false,
-                hasError: false,
-                error: null,
-            })
-            console.log({ data });
-        }
-        catch (error) {
-            setState({
-                ...state,
-                isLoading: false,
-                hasError: true,
-                error: error,
-            })
-        }
 
+const localCache = {};
+
+
+export const useFetch = ( url ) => {
+  
+  const [state, setState] = useState({
+    data: null ,
+    isLoading: true,
+    hasError: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    getFetch();
+
+  }, [url]);
+
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null,
+    });
+  }
+
+
+  const getFetch = async() => {
+
+    if ( localCache[url] ) {
+      console.log('Usando caché');
+      setState({
+        data: localCache[url],
+        isLoading: false,
+        hasError: false,
+        error: null,
+      });
+      return;
     }
-    getFecth()
-    return {
-        data: state.data,
-        isLoading: state.isLoading,
-        hasError: state.hasError,
+
+
+
+    setLoadingState();
+
+    const resp = await fetch(url);
+
+    // sleep
+    await new Promise( resolve => setTimeout(resolve, 1500) );
+
+    if ( !resp.ok ) {
+      setState({
+        data:null,
+        isLoading: false,
+        hasError: true,
+        error: {
+          code: resp.status,
+          message: resp.statusText,
+        }
+      });
+      return;
     }
+  
+    const data = await resp.json();
+    setState({
+      data: data,
+      isLoading: false,
+      hasError: false,
+      error: null,
+    })
+
+    // Manejo del caché
+    localCache[url] = data;
+
+  }
+  
+  
+
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    hasError: state.hasError,
+  }
+
 }
